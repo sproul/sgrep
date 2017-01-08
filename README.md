a9239d1098bbb45cbfcbd420bc4158c0ace824e1
Nelson Sproul <nelson.sproul@gmail.com>
2017-01-08 08:22:37 -0800

    latest from Sun Jan  8 08:22:37 PST 2017

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-various tools for unix s/w
+SGREP(1)
+
+##NAME
+	sgrep - efficiently search large ordered files
+
+##SYNOPSIS
+
+	sgrep -v SEARCH_AREA_PATTERN_START SEARCH_AREA_PATTERN_END PATTERN FILE
+
+##DESCRIPTION
+
+	<B>Sgrep</B> searches a large ordered FILE where all lines start with some key marker (e.g., a leading timestamp in a log file), limiting its search to an area bounded by the keys SEARCH_AREA_PATTERN_BEGIN and SEARCH_AREA_PATTERN_END, looking for PATTERN.  Sgrep uses a binary search to avoid the performance hit of sequentially searching the entire file as grep would normally do.
+
+##OPTIONS
+	-v verbose mode
+
+##EXAMPLES
+	
+	sgrep 20160303120205 20160303120215 /path/to/some/file /private/artifactory/logs/request.log
+
+In this case sgrep searches the artifactory log file /private/artifactory/logs/request.log for lines matching /path/to/some/file, but only looks in those lands bounded by the timestamps 20160303120205 and 20160303120215.
+config.weblog_2_tests.calibrate_and_test
--- sgrep.rb
+++ sgrep.rb
@@ -3,6 +3,9 @@ class Sgrep
         attr_accessor :beginning_of_significance
         attr_accessor :ending_of_significance
         attr_accessor :fn
+        # if both beginning_of_significance and ending_of_significance are numerical, then consider all non-numerical line beginnings to indicate a multi-line log
+        # entry (e.g., a stacktrace)
+        attr_accessor :all_lines_begin_with_digits
         attr_accessor :f
         def initialize(beginning_of_significance, ending_of_significance, patt, fn)
                 self.beginning_of_significance = beginning_of_significance
@@ -13,14 +16,19 @@ class Sgrep
                         STDERR.puts "sgrep: #{fn}: No such file or directory"
                         exit(1)
                 end
-                self.f = File.open(fn, "r")
-                puts "sgrep looking for \"#{patt}\" in file #{fn}, bounded by the significant region starting with \"#{beginning_of_significance}\" and ending with \"#{ending_of_significance}\"..." if Sgrep.trace
+                self.f = File.open(fn, "r", :encoding => "BINARY")
+                self.log "sgrep looking for \"#{patt}\" in file #{fn}, bounded by the significant region starting with \"#{beginning_of_significance}\" and ending with \"#{ending_of_significance}\"..."
                 #        header = fh.readline
                 # Process the header
                 #     while(line = fh.gets) != nil
                 #         #do stuff
                 #     end
-                # end"@@
+                # end"
+        end
+        def log(x)
+                if Sgrep.trace
+                        STDERR.puts x
+                end
         end
         def search_sequentially_from(pos)
                 if pos > 0
@@ -32,9 +40,11 @@ class Sgrep
                 while !self.f.eof? do
                         next_line_start = self.f.tell
                         line = self.f.gets
+                        # if beginning_of_significance is not unique on lines, we may be missing significant lines above.
+                        # Normally I feel okay about this in that the boundary is likely to be chosen to be a least one second before the true window of interest.
                         if line.start_with?(self.beginning_of_significance) || line > self.beginning_of_significance
                                 f.seek(next_line_start, IO::SEEK_SET)
-                                puts "searched sequentially to #{next_line_start} (seeing #{line})" if Sgrep.trace
+                                self.log "searched sequentially to #{next_line_start} (seeing #{line})"
                                 return
                         end
                 end
@@ -42,26 +52,28 @@ class Sgrep
         def seek_beginning_of_significance()
                 lower_bound = 0
                 upper_bound = File.size(self.fn)
-                puts "lower_bound=#{lower_bound}, upper_bound=#{upper_bound}" if Sgrep.trace
+                self.log "lower_bound=#{lower_bound}, upper_bound=#{upper_bound}"
                 while upper_bound > lower_bound do
                         midpoint = (lower_bound + ((upper_bound - lower_bound) / 2)).to_i
                         f.seek(midpoint, IO::SEEK_SET)
                         self.seek_next_line
                         next_line_start = self.f.tell
-                        line = self.f.gets
-                        puts "see #{line.chomp}, lower_bound=#{lower_bound}, upper_bound=#{upper_bound}, midpoint=#{midpoint}, next_line_start=#{next_line_start}" if Sgrep.trace
+                        self.log("about to call gets_possible_multiline")
+                        line = self.gets_possible_multiline
+                        break unless line
+                        self.log "see #{line.chomp}, lower_bound=#{lower_bound}, upper_bound=#{upper_bound}, midpoint=#{midpoint}, next_line_start=#{next_line_start}"
                         if line.start_with?(self.beginning_of_significance)
-                                puts "match" if Sgrep.trace
+                                self.log "match"
                                 if upper_bound > next_line_start
                                         upper_bound = next_line_start
                                 else
                                         break
                                 end
                         elsif line < self.beginning_of_significance
-                                puts "under" if Sgrep.trace
+                                self.log "under"
                                 lower_bound = self.f.tell+1
                         else
-                                puts "over" if Sgrep.trace
+                                self.log "over"
                                 upper_bound = midpoint-1
                         end
                 end
@@ -70,23 +82,76 @@ class Sgrep
         def seek_next_line()
                 while !self.f.eof? do
                         if self.f.getc == "\n"
+                                if self.f.eof?
+                                        return
+                                end
+                                if self.all_lines_begin_with_digits
+                                        # verify that we aren't in a multiline log msg (e.g., a stack trace)
+                                        next_char = self.f.getc
+                                        if next_char =~ /[[:digit:]]/
+                                                # assume that this is a new timestamp
+                                                self.f.seek(-1, IO::SEEK_CUR)
+                                                return
+                                        else
+                                                next
+                                        end
+                                end
                                 return
                         end
                 end
         end
-        def significant_lines
+        def gets_possible_multiline()
+                # This routine acts like gets except that if it detects a log entry which has multiple lines (e.g., a stack trace), then it
+                # appends them together into a single string
+                if !self.all_lines_begin_with_digits
+                        # all_lines_begin_with_digits usually tells us if the file's log entries begin with a numerical time stamp.  I use
+                        # this info to detect multiple line log entries (e.g., stack traces).  If all_lines_begin_with_digits is false, then
+                        # I really don't know anything about the format of the lines, so I should just pass thru to gets():
+                        x = self.f.gets
+                        log("gets_possible_multiline passed on '#{x}'")
+                        return x
+                end
+                if self.f.eof?
+                        log("gets_possible_multiline saw EOF")
+                        return nil
+                end
+                possible_multiline = self.f.gets
                 while !self.f.eof? do
-                        line = f.gets
-                        if line.start_with?(self.ending_of_significance) || line < self.ending_of_significance
+                        line_start_pos = self.f.tell()
+                        line = self.f.gets
+                        if line !~ /^\d/
+                                possible_multiline << line
+                        else
+                                self.f.seek(line_start_pos, IO::SEEK_SET)
+                                break
+                        end
+                end
+                log("gets_possible_multiline saw '#{possible_multiline}'")
+                return possible_multiline
+        end
+        def significant_lines
+                loop do
+                        line = self.gets_possible_multiline
+                        if !line
+                                break
+                        elsif line.start_with?(self.ending_of_significance) || line < self.ending_of_significance
+                                log "significant_lines yielding #{line}"
                                 yield line
                         else
+                                self.log "ending search now that I have seen '#{line}' (which is past '#{self.ending_of_significance}')"
                                 return
                         end
                 end
+                self.log "reached EOF without reaching self.ending_of_significance=#{self.ending_of_significance}"
                 return
         end
         def search()
-                #return `grep "#{self.patt}" #{self.fn}`
+                if self.beginning_of_significance   =~ /^\d/ && self.ending_of_significance =~ /^\d/
+                        self.log("self.all_lines_begin_with_digits = true (because of #{self.beginning_of_significance} or #{self.ending_of_significance})")
+                        self.all_lines_begin_with_digits = true
+                end
+                
+                log("search()")
                 self.seek_beginning_of_significance
 
                 exit_code = 1
